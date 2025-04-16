88ed9292b4a61089de54ad2eab2595860ee2d205
jesjack25.03 <jesjack25.03@gmail.com>
2025-04-16 02:11:44 +0000

    dwq

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,7 @@
 plugins {
     id("org.jetbrains.kotlin.plugin.compose")
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
 }
-
+    
 android {
     namespace = "com.example.msthatudykotlinedition"
     compileSdk = 35
